72cac009b167ca1914a2f1d6ed9bcfd355a98a07
Robel Kebede <robel.kebede@bison.howard.edu>
2024-09-10 13:51:25 -0400

    Fix runApp

--- runApp.sh
+++ runApp.sh
@@ -6,7 +6,7 @@ pip3 install requirements.txt
 # Check if OPENAI_API_KEY is set
 if [ -z "$OPENAI_API_KEY" ]; then
     # If OPENAI_API_KEY is not set, export the value
-    export OPENAI_API_KEY="sk-IcZSPnqo7v8mbaiE5w3bg0tx1YSV_oUPIDZYrABAcMT3BlbkFJp5wYx99-JmF0CdZVdqaFKljQMlCrkSXsSNHWg3HE4A"
+    export OPENAI_API_KEY="YOUR_API_KEY"
     echo "OPENAI_API_KEY was not set, exporting a new key."
 fi
 
